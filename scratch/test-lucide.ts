1bec273a9e76f17b33e0167aa1269add009df260
Michael Tunwashe <motrenewed@gmail.com>
2026-06-27 11:46:38 +0100

    deleted scratch

--- scratch/test-lucide.ts
+++
@@ -1,5 +0,0 @@
-import { icons, createElement } from "lucide";
-
-const calculatorIcon = icons.Calculator;
-const element = createElement(calculatorIcon);
-console.log("element outerHTML:", element.outerHTML);
